4e80a26b857c4c06bf604369fdb3f29f562fa72f
Tyler Kvochick <tymokvo@gmail.com>
2022-06-06 11:40:59 -0400

    Add fsharp slack dep example

---
+++ fsharp/slack/deps.fsx
@@ -0,0 +1,16 @@
+type Dep<'a> =
+    abstract member Get: 'a
+
+type Deps<'a, 'b>(a: Dep<'a>, b: Dep<'b>) =
+    member z.A = a
+    member z.B = b
+
+let a =
+    { new Dep<int> with
+        member z.Get = 1 }
+
+let b =
+    { new Dep<char> with
+        member z.Get = 'b' }
+
+let deps = Deps(a, b)
